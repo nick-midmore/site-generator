a55c2987619303eab7067351d0f2dc00662e2731
nick-midmore <nmidmore@gmail.com>
2025-03-06 15:14:45 +0000

    add tests for delimiter function and updated to search recursively for multiple delimiter pairs

--- src/splitdelimiter.py
+++ src/splitdelimiter.py
@@ -5,22 +5,40 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for n in old_nodes:
         if n.text_type == TextType.TEXT:
-            first_split = n.text.split(delimiter, 1)
-            if len(first_split) > 1:
-                initial_text = first_split[0]
-                remainder_text = first_split[1]
-                second_split = remainder_text.split(delimiter, 1)
-                if len(second_split) > 1:
-                    enclosed_text = second_split[0]
-                    end_text = second_split[1]
+            split = n.text.split(delimiter, 2)
+            if len(split) == 3:
+                start_text = split[0]
+                middle_text = split[1]
+                end_text = split[2]
+                if not start_text:
                     new_nodes.extend(
-                        [ 
-                            TextNode(initial_text, TextType.TEXT),
-                            TextNode(enclosed_text, text_type),
+                        [
+                            TextNode(middle_text, text_type),
                             TextNode(end_text, TextType.TEXT)
-                        ])
+                        ]
+                    )
+                elif not end_text:
+                    new_nodes.extend(
+                        [
+                            TextNode(start_text, TextType.TEXT),
+                            TextNode(middle_text, text_type)
+                        ]
+                    )
                 else:
-                    raise Exception("No closing delimiter found - invalid markdown")
+                    result_nodes = [
+                            TextNode(start_text, TextType.TEXT),
+                            TextNode(middle_text, text_type),
+                        ]
+                    result_nodes.extend(split_nodes_delimiter([TextNode(end_text, TextType.TEXT)], delimiter, text_type))
+                    new_nodes.extend(result_nodes)
+                    #     [
+                    #         TextNode(start_text, TextType.TEXT),
+                    #         TextNode(middle_text, text_type),
+                    #         TextNode(end_text, TextType.TEXT)
+                    #     ]
+                    # )
+            elif len(split) == 2:
+                raise Exception("No closing delimiter found - invalid markdown")
             else:
                 new_nodes.append(n)
         else:
--- src/test_splitdelimiter.py
+++ src/test_splitdelimiter.py
@@ -14,9 +14,42 @@ class TestSplitDelimiter(unittest.TestCase):
         
         self.assertTrue("No closing delimiter found - invalid markdown" in str(context.exception))
     
-    # def test_type_code(self):
-    #     nodes = split_nodes_delimiter([TextNode("This text has no delimiter", TextType.TEXT)], "`", TextType.CODE)
-    #     self.assertEqual(nodes, )
+    def test_type_code(self):
+        nodes = split_nodes_delimiter([TextNode("This text has a `code block` in it", TextType.TEXT)], "`", TextType.CODE)
+        self.assertEqual(nodes, [
+            TextNode("This text has a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" in it", TextType.TEXT)
+        ])
+
+    def test_type_bold(self):
+        nodes = split_nodes_delimiter([TextNode("This text has a **bold** word", TextType.TEXT)], "**", TextType.BOLD)
+        self.assertEqual(nodes, [
+            TextNode("This text has a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT)
+        ])
+    
+    def test_delimiter_at_start(self):
+        nodes = split_nodes_delimiter([TextNode("**This text is bold** and this is not", TextType.TEXT)], "**", TextType.BOLD)
+        self.assertEqual(nodes, [
+            TextNode("This text is bold", TextType.BOLD),
+            TextNode(" and this is not", TextType.TEXT)
+        ])
+
+    def test_delimiter_at_end(self):
+        nodes = split_nodes_delimiter([TextNode("Normal and **bold**", TextType.TEXT)], "**", TextType.BOLD)
+        self.assertEqual(nodes, [
+            TextNode("Normal and ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD)
+        ])
+
+    def test_multiple_delimiter_pairs(self):
+        nodes = split_nodes_delimiter([TextNode("Normal and **bold**", TextType.TEXT)], "**", TextType.BOLD)
+        self.assertEqual(nodes, [
+            TextNode("Normal and ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD)
+        ])
 
 
 if __name__ == '__main__':
