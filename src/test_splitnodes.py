d42260c3ecc7e6e64592f91a733d7ef70402fc25
nick-midmore <nmidmore@gmail.com>
2025-03-11 11:01:56 +0000

    add test file for split nodes functions

---
+++ src/test_splitnodes.py
@@ -0,0 +1,23 @@
+import unittest
+
+from split_nodes import split_nodes_image
+from textnode import TextNode, TextType
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
